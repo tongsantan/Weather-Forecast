90b135ac9ee863fa84b35518b3f0d09825bb93f5
tongsantan <vincente53@outlook.com>
2024-07-12 21:43:14 +0800

    updated

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from src.pipeline.stage_03_model_training import ModelTrainingPipeline
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>> {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
@@ -23,7 +23,7 @@ STAGE_NAME = "Data Transformation Stage"
 
 try:
     logger.info(f"************************")
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>> {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
@@ -37,7 +37,7 @@ STAGE_NAME = "Model Training Stage"
 
 try:
     logger.info(f"********************************")
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>> {STAGE_NAME} started <<<<<<")
     obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
